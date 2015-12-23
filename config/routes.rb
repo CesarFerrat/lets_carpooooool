f5d5e8369e87f80d17736a62dd796f942f1b7805
Cesar Ferrat <cesar.ferrat1@gmail.com>
2015-12-23 14:18:00 -0800

    finished home page design and login signup design

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   end
 
   # You can have the root of your site routed with "root"
-  root 'trips#home'
+  root 'homes#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   post '/trips/form' => 'trips#form'
